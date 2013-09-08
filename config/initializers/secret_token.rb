c111ca79f318e702b86c282aa566e3ed5ed7c3ce
Rasik Wankhede <rasik.wankhede@outlook.com>
2013-09-08 23:45:19 +0530

    BookCart scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookCart::Application.config.secret_token = 'ed334aa5ff8caf52f55d0b8f693d1b2d051db7bcecddcb3bfc619fc33050008e786ce78993f421c45e5c983a17e051e601b8687d6c03842b4b8f4b033c5d0b4d'
