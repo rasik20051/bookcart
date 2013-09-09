024fe5fa38096df840fe2bc81fbe7372b102be14
Rasik Wankhede <rasik.wankhede@outlook.com>
2013-09-09 14:02:35 +0530

    Add line items to the cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,28 @@ class Product < ActiveRecord::Base
 
   validates :description,:image_url,:title, presence: true
 
-  validates :image_url, format: { with: %r{\.gif|jpg|jpeg|png)$}i,
+  validates :image_url, format: { with: %r{\.(gif|jpg|jpeg|png)$}i,
 									message: "Please upload valid image files"}
 
   validates :price,numericality: {greater_than_or_equal_to: 0.01}
 
   validates :title ,uniqueness: true
+
+  default_scope order: 'title'
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
+  def ensure_not_referenced_by_any_line_item
+
+  	if line_items.count.zero?
+  		return true
+  	else
+  		errors.add(:base,"line item present")
+  		return false
+  	end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 BookCart::Application.routes.draw do
+  resources :line_items
+
+
+  resources :carts
+
+
   get "store/index"
 
 
